b3ad2090473c74f7716afe0535f51f415794694c
Max Hacking <max@hacking.co.uk>
2019-10-17 14:59:31 +0100

    Start of debugging.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ -z "${CODECOV_TOKEN}" ]]; then
-    echo "Must set CODECOV_TOKEN in env"
-    exit 1
+#if [[ -z "${CODECOV_TOKEN}" ]]; then
+#    echo "Must set CODECOV_TOKEN in env"
+#    exit 1
+#fi
+
+if [[ -z "${GITHUB_WORKSPACE}" ]]; then
+    echo "Must set GITHUB_WORKSPACE in env"
+    exit 2
 fi
 
-cd "$GITHUB_WORKSPACE"
+#cd "$GITHUB_WORKSPACE"
 
-kcov --bash-dont-parse-binary-dir \
-     --include-path=. \
-     /var/tmp/coverage \
-     bats -t tests
-     
-cd /var/tmp/coverage
-
-/usr/local/bin/codecov
+#kcov --bash-dont-parse-binary-dir \
+#     --include-path=. \
+#     /var/tmp/coverage \
+#     bats -t tests
+#     
+#cd /var/tmp/coverage
+#
+#/usr/local/bin/codecov
