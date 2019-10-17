a12c9203f6c861d3aa382891a8949bbefce16c04
Max Hacking <max@hacking.co.uk>
2019-10-17 15:02:37 +0100

    Check CODECOV_TOKEN defined in env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-#if [[ -z "${CODECOV_TOKEN}" ]]; then
-#    echo "Must set CODECOV_TOKEN in env"
-#    exit 1
-#fi
+if [[ -z "${CODECOV_TOKEN}" ]]; then
+    echo "Must set CODECOV_TOKEN in env"
+    exit 1
+fi
 
 if [[ -z "${GITHUB_WORKSPACE}" ]]; then
     echo "Must set GITHUB_WORKSPACE in env"
     exit 2
 fi
 
-#cd "$GITHUB_WORKSPACE"
+#cd "${GITHUB_WORKSPACE}" || exit 3
 
 #kcov --bash-dont-parse-binary-dir \
 #     --include-path=. \
